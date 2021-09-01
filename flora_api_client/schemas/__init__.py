3e42fe67cbddca9e4d7027d9c570cd01dd33f9c4
tulov.alex <tulov.alex@gmail.com>
2021-08-31 22:42:11 -0700

    Small refactor

--- flora_api_client/schemas/__init__.py
+++ flora_api_client/schemas/__init__.py
@@ -1,6 +1,9 @@
 from .error import ErrorResponseSchema, ErrorSchema
 from .main import ApplicationInfoResponseSchema
-from .users import UserSchema, DataForAuthSchema, RegistrationUserSchema
+from .users import (
+    UserSchema, DataForAuthSchema, RegistrationUserSchema,
+    ConfirmDataForAuthResponseSchema, ConfirmDataForAuthRequestSchema
+)
 from .auth import AuthRequestSchema, AuthResponseSchema
 
 DATE_FORMAT = '%d.%m.%Y'
@@ -10,5 +13,6 @@ DATETIME_FORMAT = '%Y-%m-%dT%H:%M:%S'
 __all__ = (
     ErrorSchema, ErrorResponseSchema, ApplicationInfoResponseSchema,
     UserSchema, DataForAuthSchema, RegistrationUserSchema,
-    DATETIME_FORMAT, DATE_FORMAT, AuthRequestSchema, AuthResponseSchema
+    DATETIME_FORMAT, DATE_FORMAT, AuthRequestSchema, AuthResponseSchema,
+    ConfirmDataForAuthResponseSchema, ConfirmDataForAuthRequestSchema
 )
