1034997379b9456c52e0e997ba7d34640f2eeda3
tulov.alex <tulov.alex@gmail.com>
2021-09-07 03:25:23 -0700

    User schema has updated

--- flora_api_client/presentations/users.py
+++ flora_api_client/presentations/users.py
@@ -44,10 +44,9 @@ class User(BaseDataclass):
     })
     is_moderated: bool = field()
     data: Dict[str, Any] = field(default_factory=dict)
-    data_for_auth: List[DataForAuth] = field(
+    data_for_auth: Optional[List[DataForAuth]] = field(
         default_factory=list, metadata={
             "validate": [UniqueItems(), Filled()],
-            "required": True
         })
     roles: List[str] = field(default_factory=list, metadata={
         'validate': [
