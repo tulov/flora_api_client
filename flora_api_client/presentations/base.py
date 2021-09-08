0fe1f5a6efbfed581436564b58c0f2948e3a8a9e
tulov.alex <tulov.alex@gmail.com>
2021-09-08 04:36:10 -0700

    Get visible pages method has added to Pager class.

--- flora_api_client/presentations/base.py
+++ flora_api_client/presentations/base.py
@@ -19,6 +19,36 @@ class Pager(BaseDataclass):
     page: int = field(default=1)
     per_page: int = field(default=10)
 
+    @staticmethod
+    def _split_diapasons(diapason1, diapason2):
+        if set(diapason1) & set(diapason2) or diapason1[
+            len(diapason1) - 1
+        ] + 1 == diapason2[0]:
+            for x in diapason2:
+                if x not in diapason1:
+                    diapason1.append(x)
+        elif len(diapason2):
+            diapason1.append('.')
+            for x in diapason2:
+                diapason1.append(x)
+        return diapason1
+
+    def get_visible_pages(self):
+        diapason1 = [1, 2]
+        diapason2 = [self.page - 1, self.page, self.page + 1]
+        diapason3 = [self.count_pages - 1, self.count_pages]
+
+        # в каждом диапазоне убираем все, что больше чем количество страниц
+        # и меньше чем 1
+        diapason1 = [x for x in diapason1 if x <= self.count_pages]
+        diapason2 = [x for x in diapason2 if 0 < x <= self.count_pages]
+        diapason3 = [x for x in diapason3 if 0 < x <= self.count_pages]
+
+        # объединяем диапазоны
+        diapason1 = self._split_diapasons(diapason1, diapason2)
+        diapason1 = self._split_diapasons(diapason1, diapason3)
+        return diapason1
+
 
 @dataclass(frozen=True)
 class Querystring(BaseDataclass):
