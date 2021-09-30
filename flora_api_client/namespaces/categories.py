7f0277aa3fa2087d90662ca145fe37150d31a8e2
tulov.alex <tulov.alex@gmail.com>
2021-09-30 02:37:57 -0700

    Method for get category by id had added

--- flora_api_client/namespaces/categories.py
+++ flora_api_client/namespaces/categories.py
@@ -3,7 +3,7 @@ from typing import Union
 
 from flora_api_client.utils.decorators import expectations
 from ..presentations.auth import RenewTokenResponse
-from ..presentations.base import Querystring
+from ..presentations.base import Querystring, WithFieldsQuerystring
 from ..presentations.categories import (
     CategoryResponse, CreateCategoryRequest, CategoriesResponse
 )
@@ -29,5 +29,13 @@ class CategoriesNamespace(Namespace):
     ) -> (int, Union[CategoriesResponse, ErrorResponse],
           RenewTokenResponse):
         return await self._get(self.build_url(query_params), **kwargs)
+
+    @expectations(schema=CategoryResponseSchema)
+    async def get(
+        self, id_: int, query_params: WithFieldsQuerystring = None, **kwargs
+    ) -> (int, Union[CategoryResponse, ErrorResponse],
+          RenewTokenResponse):
+        return await self._get(
+            self.build_url(query_params, postfix_url=id_), **kwargs)
 
 
