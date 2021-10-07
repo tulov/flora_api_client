bcc1e3bae72d1315d862d4101a8786d3cae6c719
tulov.alex <tulov.alex@gmail.com>
2021-10-07 04:47:43 -0700

    Images schemas has updated.

--- flora_api_client/presentations/images.py
+++ flora_api_client/presentations/images.py
@@ -1,12 +1,30 @@
 from dataclasses import dataclass, field
+from typing import Optional
+from marshmallow.validate import OneOf
+
 from .base import BaseDataclass, SuccessResponse
+from .enums import ImageTarget
 
 
 @dataclass(frozen=True)
 class Image(BaseDataclass):
-    filename: str = field()
+    id: int = field(metadata={
+        "strict": True,
+    })
+    position: int = field(metadata={
+        "strict": True,
+    })
     path: str = field()
     url: str = field()
+    obj_id: Optional[int] = field(metadata={
+        "strict": True,
+    })
+    obj_type: Optional[str] = field(
+        metadata={
+            'validate': OneOf([r.value for r in ImageTarget]),
+        }
+    )
+    is_moderated: Optional[bool] = field()
 
 
 @dataclass(frozen=True)
@@ -16,4 +34,7 @@ class ImageResponse(SuccessResponse):
 
 @dataclass(frozen=True)
 class ImageUploadRequest(BaseDataclass):
+    position: int = field(metadata={
+        "strict": True,
+    })
     file: str = field()
