c419de9171dade62dc21ba6078108ea16220470f
tulov.alex <tulov.alex@gmail.com>
2021-10-10 22:55:03 -0700

    method all has added to products

--- flora_api_client/namespaces/products.py
+++ flora_api_client/namespaces/products.py
@@ -3,11 +3,12 @@ from typing import Union
 
 from flora_api_client.utils.decorators import expectations
 from ..presentations.auth import RenewTokenResponse
+from ..presentations.base import Querystring
 from ..presentations.products import (
-    ProductResponse, ProductRequest
+    ProductResponse, ProductRequest, ProductsResponse
 )
 from ..presentations.error import ErrorResponse
-from ..schemas import ProductResponseSchema
+from ..schemas import ProductResponseSchema, ProductsResponseSchema
 from ..namespaces.base import Namespace
 
 
@@ -22,3 +23,9 @@ class ProductsNamespace(Namespace):
           RenewTokenResponse):
         return await self._post(self.URL, json=data.as_dict(), **kwargs)
 
+    @expectations(schema=ProductsResponseSchema)
+    async def all(
+        self, query_params: Querystring = None, **kwargs
+    ) -> (int, Union[ProductsResponse, ErrorResponse],
+          RenewTokenResponse):
+        return await self._get(self.build_url(query_params), **kwargs)
