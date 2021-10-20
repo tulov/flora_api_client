810fbf42c86fb766cd7beaf0d8e6395a3fe55e34
tulov.alex <tulov.alex@gmail.com>
2021-10-20 01:38:42 -0700

    Bind city and cities methods has added to partners namespace.

--- flora_api_client/namespaces/partners.py
+++ flora_api_client/namespaces/partners.py
@@ -5,7 +5,10 @@ from flora_api_client.utils.decorators import expectations
 from ..presentations.auth import RenewTokenResponse
 from ..presentations.error import ErrorResponse
 from ..presentations.users import RegistrationUserData, User
-from ..schemas import UserSchema
+from ..presentations.partners import BindCityRequestDataclass
+from ..presentations.base import SuccessResponse
+from ..presentations.cities import CitiesResponse
+from ..schemas import UserSchema, SuccessResponseSchema, CitiesResponseSchema
 from ..namespaces.base import Namespace
 
 
@@ -18,3 +21,22 @@ class PartnersNamespace(Namespace):
         self, data: RegistrationUserData, **kwargs
     ) -> (int, Union[User, ErrorResponse], RenewTokenResponse):
         return await self._post(self.URL, json=data.as_dict(), **kwargs)
+
+    @expectations(schema=SuccessResponseSchema)
+    async def bind_city(
+        self, id_: int, data: BindCityRequestDataclass, **kwargs
+    ) -> (int, Union[SuccessResponse, ErrorResponse],
+          RenewTokenResponse):
+        return await self._post(
+            self.build_url(postfix_url=f'{id_}/cities/'),
+            json=data.as_dict(),
+            **kwargs)
+
+    @expectations(schema=CitiesResponseSchema)
+    async def cities(
+        self, id_: int, **kwargs
+    ) -> (int, Union[CitiesResponse, ErrorResponse],
+          RenewTokenResponse):
+        return await self._get(
+            self.build_url(postfix_url=f'{id_}/cities/'),
+            **kwargs)
