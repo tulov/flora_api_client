32094642f83ac4e835c86bb30b6638dd274be36f
tulov.alex <tulov.alex@gmail.com>
2021-10-20 22:11:41 -0700

    Methods for direction partner cities has added.

--- flora_api_client/namespaces/partners.py
+++ flora_api_client/namespaces/partners.py
@@ -7,8 +7,10 @@ from ..presentations.error import ErrorResponse
 from ..presentations.users import RegistrationUserData, User
 from ..presentations.partners import BindCityRequestDataclass
 from ..presentations.base import SuccessResponse, Querystring
-from ..presentations.cities import CitiesResponse
-from ..schemas import UserSchema, SuccessResponseSchema, CitiesResponseSchema
+from ..presentations.cities import CitiesResponse, CityResponse
+from ..schemas import (
+    UserSchema, SuccessResponseSchema, CitiesResponseSchema, CityResponseSchema
+)
 from ..namespaces.base import Namespace
 
 
@@ -22,10 +24,10 @@ class PartnersNamespace(Namespace):
     ) -> (int, Union[User, ErrorResponse], RenewTokenResponse):
         return await self._post(self.URL, json=data.as_dict(), **kwargs)
 
-    @expectations(schema=SuccessResponseSchema)
+    @expectations(schema=CityResponseSchema)
     async def bind_city(
         self, id_: int, data: BindCityRequestDataclass, **kwargs
-    ) -> (int, Union[SuccessResponse, ErrorResponse],
+    ) -> (int, Union[CityResponse, ErrorResponse],
           RenewTokenResponse):
         return await self._post(
             self.build_url(postfix_url=f'{id_}/cities/'),
@@ -39,4 +41,34 @@ class PartnersNamespace(Namespace):
           RenewTokenResponse):
         return await self._get(
             self.build_url(query_params, postfix_url=f'{id_}/cities/'),
+            **kwargs)
+
+    @expectations(schema=CityResponseSchema)
+    async def city_detail(
+        self, id_: int, geoname_id: int,
+        query_params: Querystring = None, **kwargs
+    ) -> (int, Union[CityResponse, ErrorResponse],
+          RenewTokenResponse):
+        return await self._get(
+            self.build_url(query_params,
+                           postfix_url=f'{id_}/cities/{geoname_id}'),
+            **kwargs)
+
+    @expectations(schema=CityResponseSchema)
+    async def update_city(
+        self, id_: int, data: BindCityRequestDataclass, **kwargs
+    ) -> (int, Union[CityResponse, ErrorResponse],
+          RenewTokenResponse):
+        return await self._put(
+            self.build_url(postfix_url=f'{id_}/cities/{data.geoname_id}'),
+            json=data.as_dict(),
+            **kwargs)
+
+    @expectations(schema=SuccessResponseSchema)
+    async def city_detail(
+        self, id_: int, geoname_id: int, **kwargs
+    ) -> (int, Union[SuccessResponse, ErrorResponse],
+          RenewTokenResponse):
+        return await self._delete(
+            self.build_url(postfix_url=f'{id_}/cities/{geoname_id}'),
             **kwargs)
