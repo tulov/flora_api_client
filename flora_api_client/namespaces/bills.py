efd209a59d0fca3e0e55286a3901cc48b0f5f997
tulov.alex <tulov.alex@gmail.com>
2022-08-11 01:57:00 -0700

    pay method has added to bills namespace

--- flora_api_client/namespaces/bills.py
+++ flora_api_client/namespaces/bills.py
@@ -4,11 +4,11 @@ from flora_api_client.utils.decorators import expectations
 from ..presentations.auth import RenewTokenResponse
 from ..presentations.base import WithFieldsQuerystring, Querystring
 from ..presentations.bills import (
-    BillsResponse, BillResponse
+    BillsResponse, BillResponse, BillPayRequest
 )
 from ..presentations.error import ErrorResponse
 from ..schemas import (
-    BillsResponseSchema, BillResponseSchema
+    BillsResponseSchema, BillResponseSchema, SuccessResponseSchema
 )
 from ..namespaces.base import Namespace
 
@@ -30,3 +30,12 @@ class BillsNamespace(Namespace):
     ) -> (int, Union[BillsResponse, ErrorResponse],
           RenewTokenResponse):
         return await self._get(self.build_url(query_params), **kwargs)
+
+    @expectations(schema=SuccessResponseSchema)
+    async def pay(
+        self, guid: str, pay_service: str, data: BillPayRequest, **kwargs
+    ) -> (int, Union[BillsResponse, ErrorResponse],
+          RenewTokenResponse):
+        return await self._post(
+            self.build_url(postfix_url=f"{guid}/pay/{pay_service}"),
+            json=data.as_dict(), **kwargs)
