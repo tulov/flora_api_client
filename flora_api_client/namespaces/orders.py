49a84187db5b5bd067ef321279a22aebdb7e56ba
tulov.alex <tulov.alex@gmail.com>
2022-10-05 20:39:22 -0700

    order reject support has added.

--- flora_api_client/namespaces/orders.py
+++ flora_api_client/namespaces/orders.py
@@ -4,7 +4,7 @@ from typing import Union
 from flora_api_client.utils.decorators import expectations
 from ..presentations.auth import RenewTokenResponse
 from ..presentations.base import (
-    WithFieldsQuerystring, Querystring, ResultResponse
+    WithFieldsQuerystring, Querystring, ResultResponse, SuccessResponse
 )
 from ..presentations.orders import (
     OrderResponse, CreateOrderRequest, OrdersResponse, OrderCommentBase,
@@ -13,7 +13,7 @@ from ..presentations.orders import (
 from ..presentations.error import ErrorResponse
 from ..schemas import (
     OrderResponseSchema, OrdersResponseSchema, OrderCommentResponseSchema,
-    OrderBillResponseSchema, ResultResponseSchema
+    OrderBillResponseSchema, ResultResponseSchema, SuccessResponseSchema
 )
 from ..namespaces.base import Namespace
 
@@ -71,4 +71,13 @@ class OrdersNamespace(Namespace):
           RenewTokenResponse):
         return await self._put(
             self.build_url(postfix_url=f"{order_id}/accept/"), json={},
+            **kwargs)
+
+    @expectations(schema=SuccessResponseSchema)
+    async def reject(
+        self, order_id: int, **kwargs
+    ) -> (int, Union[SuccessResponse, ErrorResponse],
+          RenewTokenResponse):
+        return await self._put(
+            self.build_url(postfix_url=f"{order_id}/reject/"), json={},
             **kwargs)
