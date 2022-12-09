48f9e3781aeca708b8ec6ed1a3f208675bba4ed9
lex <tulov.alex@gmail.com>
2022-12-09 16:06:05 +0600

    check method for promo code has added.

--- flora_api_client/presentations/promo.py
+++ flora_api_client/presentations/promo.py
@@ -20,6 +20,17 @@ class PromoCode(BaseDataclass):
     data: Dict[str, Any] = field(default_factory=dict)
     created_at: Optional[datetime] = field(default=None)
 
+    def check(self) -> bool:
+        if not self.is_valid:
+            return False
+        if 'period' in self.data:
+            cur_date = datetime.now().date()
+            arr = self.data['period'].split(" - ")
+            start = datetime.strptime(arr[0], "%d.%m.%Y")
+            end = datetime.strptime(arr[1], "%d.%m.%Y")
+            return start <= cur_date <= end
+        return True
+
 
 @dataclass(frozen=True)
 class PromoCodeResponse(SuccessResponse):
