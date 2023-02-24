c1977b6545666ae0605aebdaa531c4cecb4f78b8
lex <tulov.alex@gmail.com>
2023-02-24 12:34:17 +0600

    устранил ошибку конвертации в order если при запросе происходила ошибка.

--- flora_api_client/utils/decorators.py
+++ flora_api_client/utils/decorators.py
@@ -14,16 +14,21 @@ def expectations(*, schema, expected_code=HTTPStatus.OK):
         async def inner(*args, **kwargs):
             code, res, new_tokens = await fn(*args, **kwargs)
             sch = schema() if code == expected_code else ErrorResponseSchema()
-            tokens = RenewTokenResponseSchema().load(
-                new_tokens, unknown=EXCLUDE
-            ) if new_tokens else None
-            if schema == OrderResponseSchema:
+            tokens = (
+                RenewTokenResponseSchema().load(new_tokens, unknown=EXCLUDE)
+                if new_tokens
+                else None
+            )
+            if type(sch, OrderResponseSchema):
                 o = sch.load(res, unknown=EXCLUDE)
                 order_schema = OrderSchema()
                 for i in range(0, len(o.result.children)):
                     o.result.children[i] = order_schema.load(
-                        o.result.children[i], unknown=EXCLUDE)
+                        o.result.children[i], unknown=EXCLUDE
+                    )
                 return code, o, tokens
             return code, sch.load(res, unknown=EXCLUDE), tokens
+
         return inner
+
     return decorate
