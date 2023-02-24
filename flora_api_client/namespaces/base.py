b19ab95c545442bc217c4dd6c0c64c3b1673e557
lex <tulov.alex@gmail.com>
2023-02-24 16:14:57 +0600

    json payload has updated for http client

--- flora_api_client/namespaces/base.py
+++ flora_api_client/namespaces/base.py
@@ -1,11 +1,11 @@
-from typing import Dict, Any, Union
-from urllib.parse import urlencode
 from http import HTTPStatus
-from simplejson import dumps
+from typing import Any
+from urllib.parse import urlencode
 
 from aiohttp import ClientSession
 
-from flora_api_client.presentations.base import Querystring, BaseDataclass
+from flora_api_client.payloads import dumps
+from flora_api_client.presentations.base import BaseDataclass
 
 
 class Namespace:
@@ -16,24 +16,21 @@ class Namespace:
         self._signer = signer
         self._url_prefix = url_prefix
 
-    def get_auth_headers(self, body: Dict[str, Any]) -> Dict[str, str]:
+    def get_auth_headers(self, body: dict[str, Any]) -> dict[str, str]:
         return {
-            'X-Request-Sign': self._signer.get_sign(body),
-            'X-Request-App': self._signer.public_key
+            "X-Request-Sign": self._signer.get_sign(body),
+            "X-Request-App": self._signer.public_key,
         }
 
-    async def _run_query(self, url, method="get", *,
-                         long_token: str = "", **kwargs):
+    async def _run_query(self, url, method="get", *, long_token: str = "", **kwargs):
         async with ClientSession(json_serialize=dumps) as session:
             m = getattr(session, method)
-            async with m(
-                f'{self._host}{self._url_prefix}{url}', **kwargs
-            ) as resp:
+            async with m(f"{self._host}{self._url_prefix}{url}", **kwargs) as resp:
                 # проверяем на необходимость обновления токена
                 if resp.status != HTTPStatus.FORBIDDEN:
                     return resp.status, await resp.json(), None
                 body = await resp.json()
-                err = body.get('error', {})
+                err = body.get("error", {})
                 err_code = err.get("error_code", 0)
                 first_resp_status = resp.status
                 if err_code != 8:
@@ -41,63 +38,64 @@ class Namespace:
             renew_body = {"token": long_token}
             renew_kwargs = {"headers": self.get_auth_headers(renew_body)}
             async with session.post(
-                f'{self._host}{self._url_prefix}/auth/renew/',
+                f"{self._host}{self._url_prefix}/auth/renew/",
                 json=renew_body,
-                **renew_kwargs
+                **renew_kwargs,
             ) as r:
                 if r.status != HTTPStatus.OK:
                     return first_resp_status, body, None
                 new_tokens = await r.json()
             # повторяем запрос с новым токеном
-            kwargs['headers']['X-Auth-Token'] = new_tokens['token']
-            async with m(
-                f'{self._host}{self._url_prefix}{url}', **kwargs
-            ) as resp:
+            kwargs["headers"]["X-Auth-Token"] = new_tokens["token"]
+            async with m(f"{self._host}{self._url_prefix}{url}", **kwargs) as resp:
                 return resp.status, await resp.json(), new_tokens
 
     async def _get(self, url, **kwargs):
-        headers = self.get_auth_headers({"url": f'{self._url_prefix}{url}'})
-        if 'headers' in kwargs:
-            kwargs['headers'].update(headers)
+        headers = self.get_auth_headers({"url": f"{self._url_prefix}{url}"})
+        if "headers" in kwargs:
+            kwargs["headers"].update(headers)
         else:
-            kwargs['headers'] = headers
+            kwargs["headers"] = headers
         return await self._run_query(url, **kwargs)
 
     async def _post(self, url, **kwargs):
-        headers = self.get_auth_headers(kwargs.get('json', {}))
-        if 'headers' in kwargs:
-            kwargs['headers'].update(headers)
+        headers = self.get_auth_headers(kwargs.get("json", {}))
+        if "headers" in kwargs:
+            kwargs["headers"].update(headers)
         else:
-            kwargs['headers'] = headers
-        return await self._run_query(url, 'post', **kwargs)
+            kwargs["headers"] = headers
+        return await self._run_query(url, "post", **kwargs)
 
     async def _put(self, url, **kwargs):
-        headers = self.get_auth_headers(kwargs.get('json', {}))
-        if 'headers' in kwargs:
-            kwargs['headers'].update(headers)
+        headers = self.get_auth_headers(kwargs.get("json", {}))
+        if "headers" in kwargs:
+            kwargs["headers"].update(headers)
         else:
-            kwargs['headers'] = headers
-        return await self._run_query(url, 'put', **kwargs)
+            kwargs["headers"] = headers
+        return await self._run_query(url, "put", **kwargs)
 
     async def _delete(self, url, **kwargs):
-        headers = self.get_auth_headers({"url": f'{self._url_prefix}{url}'})
-        if 'headers' in kwargs:
-            kwargs['headers'].update(headers)
+        headers = self.get_auth_headers({"url": f"{self._url_prefix}{url}"})
+        if "headers" in kwargs:
+            kwargs["headers"].update(headers)
         else:
-            kwargs['headers'] = headers
-        return await self._run_query(url, 'delete', **kwargs)
+            kwargs["headers"] = headers
+        return await self._run_query(url, "delete", **kwargs)
 
-    def build_url(self,
-                  query_params: Union[BaseDataclass, Dict[str, Any]] = None, *,
-                  postfix_url: Union[str, int] = '', url: str = None):
-        query_string = ''
+    def build_url(
+        self,
+        query_params: BaseDataclass | dict[str, Any] = None,
+        *,
+        postfix_url: str | int = "",
+        url: str = None,
+    ):
+        query_string = ""
         if query_params:
-            d = query_params.as_dict() \
-                if type(query_params) != dict else query_params
+            d = query_params.as_dict() if type(query_params) != dict else query_params
             p = {key: d[key] for key in d if d[key] is not None}
-            query_string = f'?{urlencode(p)}'
-            if query_string == '?':
-                query_string = ''
+            query_string = f"?{urlencode(p)}"
+            if query_string == "?":
+                query_string = ""
         if url is None:
             url = self.URL
-        return f'{url}{postfix_url}{query_string}'
+        return f"{url}{postfix_url}{query_string}"
