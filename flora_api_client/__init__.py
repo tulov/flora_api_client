5924f59a3f49db5219ea540f2ad65c9eb516b263
lex <tulov.alex@gmail.com>
2023-03-09 12:32:23 +0600

    изменил алгоритм подписи запросов для работы с русским текстом

--- flora_api_client/__init__.py
+++ flora_api_client/__init__.py
@@ -6,7 +6,7 @@ __maintainer__ = __author__
 
 __email__ = "tulov.alex@google.com"
 __license__ = "MIT"
-__version__ = "0.6.27"
+__version__ = "0.6.28"
 
 
 __all__ = (
--- flora_api_client/auth/singer.py
+++ flora_api_client/auth/singer.py
@@ -1,19 +1,20 @@
-import json
-from typing import Mapping, List, Union, Dict, Any
+from collections.abc import Mapping
 from hashlib import sha256
+from typing import Any
+from urllib.parse import unquote
 
 
-def _add(res: Dict[str, Any], key: str, k, v):
-    n_key = f'{key}.{k}' if key else f'{k}'
-    if isinstance(v, List):
+def _add(res: dict[str, Any], key: str, k, v):
+    n_key = f"{key}.{k}" if key else f"{k}"
+    if isinstance(v, list):
         res.update(_inline_list(n_key, v))
     elif isinstance(v, Mapping):
         res.update(_inline_mapping(n_key, v))
     else:
-        res[n_key] = f'{v}'
+        res[n_key] = f"{v}"
 
 
-def _inline_list(key: str, d: List) -> Mapping:
+def _inline_list(key: str, d: list) -> Mapping:
     res = {}
     for k, v in enumerate(d):
         _add(res, key, k, v)
@@ -27,29 +28,29 @@ def _inline_mapping(key: str, d: Mapping) -> Mapping:
     return res
 
 
-def inline(d: Union[Mapping, List]) -> Mapping:
+def inline(d: Mapping | list) -> Mapping:
     result = {}
     if not d:
         return result
-    if isinstance(d, List):
-        result.update(_inline_list('', d))
+    if isinstance(d, list):
+        result.update(_inline_list("", d))
     elif isinstance(d, Mapping):
-        result.update(_inline_mapping('', d))
+        result.update(_inline_mapping("", d))
     return result
 
 
 class Singer:
     def __init__(self, *, private_key: str, public_key: str):
-        self.private_key = private_key,
+        self.private_key = (private_key,)
         self.public_key = public_key
 
     def get_sign(self, body: Mapping) -> str:
         # все словари и массивы приводим к одномерному виду
         params = inline(body)
-        # сортируем
-        sorted_keys = list(sorted(params.keys()))
-        # конкатенируем значения
-        concatenated_values = ''.join([params[k] for k in sorted_keys])
-        concatenated_values = f'{self.private_key}{concatenated_values}'
+        # сортируем и конкатенируем значения
+        concatenated_values = "".join(
+            [unquote(params[k]) for k in sorted(params.keys())]
+        )
+        concatenated_values = f"{self.private_key}{concatenated_values}"
         # вычисляем хеш sha256
         return sha256(concatenated_values.encode()).hexdigest()
