6478df13e06052d30d49e605a5a74ab72989a9da
lex <tulov.alex@gmail.com>
2023-04-10 15:20:45 +0600

    добавил схему для класса PriceBase

--- flora_api_client/schemas/prices.py
+++ flora_api_client/schemas/prices.py
@@ -1,25 +1,22 @@
 import marshmallow_dataclass
 from flora_api_client.presentations.prices import (
-    PriceResponse, PricesResponse, PricesRequest, PricesCurrentResponse,
-    PricesCurrentQuerystring
+    PriceResponse,
+    PricesResponse,
+    PricesRequest,
+    PricesCurrentResponse,
+    PricesCurrentQuerystring,
+    PriceBase,
 )
 from marshmallow import Schema, EXCLUDE
 
 Schema.Meta.unknown = EXCLUDE
 
 
-PriceResponseSchema = marshmallow_dataclass.class_schema(
-    PriceResponse
-)
-PricesResponseSchema = marshmallow_dataclass.class_schema(
-    PricesResponse
-)
-PricesRequestSchema = marshmallow_dataclass.class_schema(
-    PricesRequest
-)
-PricesCurrentResponseSchema = marshmallow_dataclass.class_schema(
-    PricesCurrentResponse
-)
+PriceResponseSchema = marshmallow_dataclass.class_schema(PriceResponse)
+PricesResponseSchema = marshmallow_dataclass.class_schema(PricesResponse)
+PricesRequestSchema = marshmallow_dataclass.class_schema(PricesRequest)
+PricesCurrentResponseSchema = marshmallow_dataclass.class_schema(PricesCurrentResponse)
 PricesCurrentQuerystringSchema = marshmallow_dataclass.class_schema(
     PricesCurrentQuerystring
 )
+PriceBaseSchema = marshmallow_dataclass.class_schema(PriceBase)
