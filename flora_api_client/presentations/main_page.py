0abdb94332d388a90b94a31d16cd158e4324ec13
lex <tulov.alex@gmail.com>
2023-06-23 09:33:34 +0600

    добавил идентификатор категории в блоки для главной страницы

--- flora_api_client/presentations/main_page.py
+++ flora_api_client/presentations/main_page.py
@@ -10,6 +10,7 @@ class MainPageBlock(BaseDataclass):
     slug: str = field(metadata={"validate": Length(max=25, min=1)})
     title: str | None = field(metadata={"validate": Length(max=100)})
     description: str | None = field(metadata={"validate": Length(max=1000)})
+    category_id: int | None = field()
     filters: str | None = field(metadata={"validate": Length(max=1000)})
     sorts: str | None = field(metadata={"validate": Length(max=1000)})
     is_system: bool = field()
