c4d314f5c2af6cf7c8126d046410ffb125f2cb4e
lex <tulov.alex@gmail.com>
2023-08-18 15:56:19 +0600

    Добавил метод для получения ид пользователей по номеру заказа.

--- flora_api_client/namespaces/chats.py
+++ flora_api_client/namespaces/chats.py
@@ -1,8 +1,10 @@
 from flora_api_client.utils.decorators import expectations
 from ..namespaces.base import Namespace
 from ..presentations.auth import RenewTokenResponse
+from ..presentations.base import ResultResponse
 from ..presentations.chats import ChatResponse
 from ..presentations.error import ErrorResponse
+from ..schemas import ResultResponseSchema
 from ..schemas.chats import ChatResponseSchema
 
 
@@ -14,3 +16,11 @@ class ChatsNamespace(Namespace):
         self, **kwargs
     ) -> (int, ChatResponse | ErrorResponse, RenewTokenResponse):
         return await self._get(self.build_url(postfix_url="url/"), **kwargs)
+
+    @expectations(schema=ResultResponseSchema)
+    async def users(
+        self, order_id, **kwargs
+    ) -> (int, ResultResponse | ErrorResponse, RenewTokenResponse):
+        return await self._get(
+            self.build_url(postfix_url=f"order/{order_id}/users/"), **kwargs
+        )
